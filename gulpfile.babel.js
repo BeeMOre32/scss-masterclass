1eae44591efbdcfab3ce793ad5dcd3dd9b41b860
BeeMOre32 <windowssart01@gmail.com>
2022-05-09 13:22:03 +0900

    updatecode

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
   css: {
     watch: "src/scss/*",
-    src: "src/scss/fourth.scss",
+    src: "src/scss/sixth.scss",
     dest: "dist/css",
   },
 };
